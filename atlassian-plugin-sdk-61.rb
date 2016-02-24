6166d23ea3061dc1422659509e58a9657658b18b
David Simpson <david@davidsimpson.me>
2016-02-24 19:44:34 +0000

    Create atlassian-plugin-sdk-61.rb

---
+++ atlassian-plugin-sdk-61.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+class AtlassianPluginSdk61 < Formula
+  homepage 'https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation'
+  url 'https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/amps/atlassian-plugin-sdk/6.1.0/atlassian-plugin-sdk-6.1.0.tar.gz'
+  sha1 '609c995fefe0d6abaf9f50759f066426475bd57c'
+
+  def install
+    # Remove windows files
+    rm_f Dir["bin/*.bat"]
+
+    # Install jars in libexec to avoid conflicts
+    libexec.install Dir['*']
+
+    # Symlink binaries
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  def caveats
+    <<-EOS.undent
+      Thanks for installing the Atlassian Plugin SDK. For more information,
+      visit https://developer.atlassian.com.
+
+      To create a plugin skeleton using atlas-create-APPLICATION-plugin, e.g.:
+        atlas-create-jira-plugin or atlas-create-confluence-plugin
+
+      To run your plugin's host application with the plugin skeleton installed:
+        atlas-run
+    EOS
+  end
+end
