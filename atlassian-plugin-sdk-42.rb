467caccba9061b98c44abf9131085f1056aad153
David Simpson <david@davidsimpson.me>
2016-02-24 19:28:08 +0000

    Use rawgit to host SDK v4.2.20 to ensure it doesn't get lost

--- atlassian-plugin-sdk-42.rb
+++ atlassian-plugin-sdk-42.rb
@@ -2,7 +2,8 @@ require 'formula'
 
 class AtlassianPluginSdk42 < Formula
   homepage 'https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation'
-  url 'http://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/amps/atlassian-plugin-sdk/4.2.20/atlassian-plugin-sdk-4.2.20.tar.gz'
+  # url 'http://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/amps/atlassian-plugin-sdk/4.2.20/atlassian-plugin-sdk-4.2.20.tar.gz'
+  url 'https://cdn.rawgit.com/dvdsmpsn/homebrew-tap/master/repository-mirror/altassian-plugin-sdk/4.2.20/atlassian-plugin-sdk-4.2.20.tar.gz'
   sha1 '550eee313dd00483ab66c281791da60389f0ef6e'
 
   def install
