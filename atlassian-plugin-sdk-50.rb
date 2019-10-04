18b80ecb9e910046783613957c4c0d8277331983
David Simpson <david@davidsimpson.me>
2019-10-04 14:06:41 +0100

    Update atlassian-plugin-sdk-50.rb
    Update the download link

--- atlassian-plugin-sdk-50.rb
+++ atlassian-plugin-sdk-50.rb
@@ -4,7 +4,8 @@ class AtlassianPluginSdk50 < Formula
   homepage 'https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation'
   url 'https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/amps/atlassian-plugin-sdk/5.0.4/atlassian-plugin-sdk-5.0.4.tar.gz'
   # If the file goes missing from above, swap out for the one mirrored in this repo:
-  mirror 'https://cdn.rawgit.com/dvdsmpsn/homebrew-tap/master/repository-mirror/atlassian-plugin-sdk/5.0.4/atlassian-plugin-sdk-5.0.4.tar.gz'
+  # mirror 'https://cdn.rawgit.com/dvdsmpsn/homebrew-tap/master/repository-mirror/atlassian-plugin-sdk/5.0.4/atlassian-plugin-sdk-5.0.4.tar.gz'
+  mirror 'https://github.com/dvdsmpsn/homebrew-tap/raw/master/repository-mirror/atlassian-plugin-sdk/5.0.4/atlassian-plugin-sdk-5.0.4.tar.gz'
   sha256 'd4381986d3c44a39b7b896ce270598d6980ae79f26f51f29cb367f80ab26f74e'
 
   def install
